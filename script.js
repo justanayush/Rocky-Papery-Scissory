0511c7e03d4336572a044014dc1f95d8b4c02c24
VishAyush <139580532+INFERNO1211@users.noreply.github.com>
2024-05-13 00:00:08 +0530

    wrote more js

--- script.js
+++ script.js
@@ -12,19 +12,37 @@ function shake(){
     cpuResult.classList.add('shake');
 }
 
+button.addEventListener('click',()=>{
+    gamePlay()
+})
 
-function gamePlay(src){
+function gamePlay(source){
+
     let randNum = Math.floor(Math.random() * 3);
-    const source = src;
+    const url = arr[randNum]
     button.addEventListener('click', () => {
         shake();
-        setTimeout((source) => {
+        setTimeout(() => {
             userResult.classList.remove('shake');
             cpuResult.classList.remove('shake');
             console.log(source)
-            userImg.setAttribute('src', source);
+            userImg.setAttribute('src',source)
+            cpuImg.setAttribute('src',url)
+            conditions();
         },2000)
     })
+}
+
+function conditions(){
+    if (userImg.getAttribute('src') === cpuImg.getAttribute('src')){
+        button.innerText = "It's a drawww ;( \n Tap to Restart";
+    
+    }else if((userImg.getAttribute('src','images/rock.png') && cpuImg.getAttribute('src','images/scissors.png')) || (userImg.getAttribute('src','scissors/rock.png') && cpuImg.getAttribute('src','images/paper.png')) || (userImg.getAttribute('src','images/paper.png') && cpuImg.getAttribute('src','images/rock.png'))){
+        button.innerText = "Congratulations :) \n You Won \n Tap to Restart ";
+    
+    }else if((userImg.getAttribute('src','images/rock.png') && cpuImg.getAttribute('src','images/paper.png')) || (userImg.getAttribute('src','scissors/rock.png') && cpuImg.getAttribute('src','images/rock.png')) || (userImg.getAttribute('src','images/paper.png') && cpuImg.getAttribute('src','images/scissors.png'))){
+        button.innerText = "Oh! , you've lost \n Please Try Again...";
+    }
 }
 
 rock.addEventListener('click', () => {
@@ -34,7 +52,7 @@ rock.addEventListener('click', () => {
         scissor.classList.add('none');
         button.innerHTML = "Tap to Play !!!"
         button.style.pointerEvents = 'all';
-        gamePlay(src);
+        gamePlay("images/rock.png");
 })
 paper.addEventListener('click', () => {
         paper.style.opacity = '1';
@@ -42,7 +60,7 @@ paper.addEventListener('click', () => {
         scissor.classList.add('none');
         button.innerHTML = "Tap to Play !!!"
         button.style.pointerEvents = 'all';
-        gamePlay(this);
+        gamePlay("images/paper.png");
 })
 scissor.addEventListener('click', () => {
         scissor.style.opacity = '1';
@@ -50,7 +68,7 @@ scissor.addEventListener('click', () => {
         rock.classList.add('none');
         button.innerHTML = "Tap to Play !!!"
         button.style.pointerEvents = 'all';
-        gamePlay(this);
+        gamePlay("images/scissors.png");
 })
 
 
