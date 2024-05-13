9ce7a426cd124bf512f49f056955a7d692be7b35
VishAyush <139580532+INFERNO1211@users.noreply.github.com>
2024-05-13 20:36:47 +0530

    still working on start again functionality

--- script.js
+++ script.js
@@ -2,73 +2,132 @@ const userResult = document.querySelector('.userResult');
 const button = document.querySelector('.btn');
 const cpuResult = document.querySelector('.cpuResult');
 const rock = document.querySelector('.rock');
+const rockImg = document.querySelector('.rock-img');
+const paperImg = document.querySelector('.paper-img');
+const scissorImg = document.querySelector('.scissor-img');
 const paper = document.querySelector('.paper');
 const scissor = document.querySelector('.scissor');
 const userImg = document.querySelector('.userImg');
 const cpuImg = document.querySelector('.cpuImg');
-const arr = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+const winLoss = document.querySelector('.win-loss');
 function shake(){
     userResult.classList.add('shake');
     cpuResult.classList.add('shake');
 }
-
-button.addEventListener('click',()=>{
-    gamePlay()
-})
-
-function gamePlay(source){
-
+rock.classList.add('options');
+paper.classList.add('options');
+scissor.classList.add('options');
+const arr = ["images/rock.png", "images/paper.png", "images/scissor.png"];
+function gamePlay(){
     let randNum = Math.floor(Math.random() * 3);
-    const url = arr[randNum]
-    button.addEventListener('click', () => {
+    
+    winLoss.innerText = '';
+    winLoss.style.display = 'none';
+    button.innerText = "Select an Option";
+    button.style.display = 'block';
+    button.style.pointerEvents = 'none';
+rock.addEventListener('click',() => {
+    const url = rockImg.getAttribute('src');
+    rock.style.opacity = '1';
+    button.style.pointerEvents = 'all';
+    paper.style.pointerEvents = 'none';
+    scissor.style.pointerEvents = 'none';
+    button.innerText = "Tap to Play";
+    button.addEventListener('click',()=>{
         shake();
-        setTimeout(() => {
+        setTimeout(()=>{
             userResult.classList.remove('shake');
             cpuResult.classList.remove('shake');
-            console.log(source)
-            userImg.setAttribute('src',source)
-            cpuImg.setAttribute('src',url)
-            conditions();
+            userImg.setAttribute('src',url);
+            cpuImg.setAttribute('src',arr[randNum]);
+            const output = conditions[obj[url] + obj[arr[randNum]]];
+            button.style.display = 'none';
+            winLoss.innerText = output;
+            winLoss.style.display = 'block';
+            winLoss.addEventListener('click',()=>{
+                userImg.setAttribute('src',"images/rock.png");
+                cpuImg.setAttribute('src',"images/rock.png");
+                randNum = Math.floor(Math.random() * 3);
+                gamePlay();
+            })
         },2000)
     })
-}
-
-function conditions(){
-    if (userImg.getAttribute('src') === cpuImg.getAttribute('src')){
-        button.innerText = "It's a drawww ;( \n Tap to Restart";
+   
+})
+paper.addEventListener('click',() => {
+    const url = paperImg.getAttribute('src');
+    paper.style.opacity = '1';
+    button.style.pointerEvents = 'all';
+    rock.style.pointerEvents = 'none';
+    scissor.style.pointerEvents = 'none';
+    button.innerText = "Tap to Play";
+    button.addEventListener('click',()=>{
+        shake();
+        setTimeout(()=>{
+            userResult.classList.remove('shake');
+            cpuResult.classList.remove('shake');
+            userImg.setAttribute('src',url);
+            cpuImg.setAttribute('src',arr[randNum]);
+            const output = conditions[obj[url] + obj[arr[randNum]]];
+            button.innerText = output;
+            button.style.display = 'none';
+            winLoss.innerText = output;
+            winLoss.style.display = 'block';
+            winLoss.addEventListener('click',()=>{
+                userImg.setAttribute('src',"images/rock.png");
+                cpuImg.setAttribute('src',"images/rock.png");
+                randNum = Math.floor(Math.random() * 3);
+                gamePlay();
+            })
+        },2000)
+    })
     
-    }else if((userImg.getAttribute('src','images/rock.png') && cpuImg.getAttribute('src','images/scissors.png')) || (userImg.getAttribute('src','scissors/rock.png') && cpuImg.getAttribute('src','images/paper.png')) || (userImg.getAttribute('src','images/paper.png') && cpuImg.getAttribute('src','images/rock.png'))){
-        button.innerText = "Congratulations :) \n You Won \n Tap to Restart ";
+})
+scissor.addEventListener('click',() => {
+    const url = scissorImg.getAttribute('src');
+    scissor.style.opacity = '1';
+    button.style.pointerEvents = 'all';
+    rock.style.pointerEvents = 'none';
+    paper.style.pointerEvents = 'none';
+    button.innerText = "Tap to Play";
+    button.addEventListener('click',()=>{
+        shake();
+        setTimeout(()=>{
+            userResult.classList.remove('shake');
+            cpuResult.classList.remove('shake');
+            userImg.setAttribute('src',url);
+            cpuImg.setAttribute('src',arr[randNum]);
+            const output = conditions[obj[url] + obj[arr[randNum]]];
+            button.innerText = output;
+            button.style.display = 'none';
+            winLoss.innerText = output;
+            winLoss.style.display = 'block';
+            winLoss.addEventListener('click',()=>{
+                userImg.setAttribute('src',"images/rock.png");
+                cpuImg.setAttribute('src',"images/rock.png");
+                randNum = Math.floor(Math.random() * 3);
+                gamePlay();
+            })
+        },2000)
+    })
     
-    }else if((userImg.getAttribute('src','images/rock.png') && cpuImg.getAttribute('src','images/paper.png')) || (userImg.getAttribute('src','scissors/rock.png') && cpuImg.getAttribute('src','images/rock.png')) || (userImg.getAttribute('src','images/paper.png') && cpuImg.getAttribute('src','images/scissors.png'))){
-        button.innerText = "Oh! , you've lost \n Please Try Again...";
-    }
+})
+
 }
-
-rock.addEventListener('click', () => {
-        const src = arr[0];
-        rock.style.opacity = '1';
-        paper.classList.add('none');
-        scissor.classList.add('none');
-        button.innerHTML = "Tap to Play !!!"
-        button.style.pointerEvents = 'all';
-        gamePlay("images/rock.png");
-})
-paper.addEventListener('click', () => {
-        paper.style.opacity = '1';
-        rock.classList.add('none');
-        scissor.classList.add('none');
-        button.innerHTML = "Tap to Play !!!"
-        button.style.pointerEvents = 'all';
-        gamePlay("images/paper.png");
-})
-scissor.addEventListener('click', () => {
-        scissor.style.opacity = '1';
-        paper.classList.add('none');
-        rock.classList.add('none');
-        button.innerHTML = "Tap to Play !!!"
-        button.style.pointerEvents = 'all';
-        gamePlay("images/scissors.png");
-})
-
-
+const obj = {
+    "images/rock.png":"R",
+    "images/paper.png":"P",
+    "images/scissor.png":"S",
+}
+const conditions = {
+    "SS": "Its a Drawww ! \n Tap to Play Again !",
+    "RR": "Its a Drawww ! \n Tap to Play Again !",
+    "PP": "Its a Drawww ! \n Tap to Play Again !",
+    "RS": "You Won :) \n Tap to Play Again !",
+    "SP": "You Won :) \n Tap to Play Again !",
+    "PR": "You Won :) \n Tap to Play Again !",
+    "RP": "You Lose \n Try Again ;)",
+    "PS": "You Lose \n Try Again ;)",
+    "PR": "You Lose \n Try Again ;)"
+}
+gamePlay();
